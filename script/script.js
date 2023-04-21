376bc55a571dee66603cb21d0647479df2c2adba
Annita Maniri <annitk@gmail.com>
2023-04-21 12:17:51 +0300

    add Style

---
+++ script/script.js
@@ -0,0 +1,10 @@
+window.onload = init;
+function init() {
+    var button = document.getElementById("addButton")
+    button.onclick = submit;
+}
+var button = getElementById("addButton");
+function submit() {
+    alert("You subscribed to the axalotl newsletter")
+}
+button.onclick = send;
